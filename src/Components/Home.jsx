1c3c7e5b5c9f5ef3b29a6a3ccf1fadc59575e885
Niranjan <narayana9492324464@gmail.com>
2022-07-16 13:58:31 +0530

    sidenavbar added

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,22 +2,27 @@ import { useEffect, useState } from "react";
 import DispalyDataCard from "../utils/DisPlayCard";
 import Loading from "../utils/Loading";
 import NavbarTop from "../utils/Navbar";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import { green, red } from "@mui/material/colors";
 
 export default function Home(params) {
-    const [news,Setnews]= useState([])
-    const [def, Setdef] = useState("top-headlines");
-    const [loading,Setloading]= useState(false)
-    const [searchInput, SetsearchInput] = useState('')
-    function SetSearchInput(value){
-        SetsearchInput(value)
-    }
+	// const [news,Setnews]= useState([])
+	// const [def, Setdef] = useState("top-headlines");
+	// const [loading,Setloading]= useState(false)
+	// const [searchInput, SetsearchInput] = useState('')
+	// function SetSearchInput(value){
+	//     SetsearchInput(value)
+	// }
 
-
-
-    return (
+	return (
 		<div>
-			<NavbarTop callback={SetSearchInput} />
+			<NavbarTop />
 			
 		</div>
 	);
-};
+}
--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -1,10 +1,11 @@
 import { Routes, Route, Link } from "react-router-dom";
+import Home from "../Components/Home";
 import DispalyeCard from "../utils/DisPlayCard";
 export default function AllRoutes() {
 	return (
 		<Routes>
 			<Route path="/" element={<DispalyeCard/>} />
-			{/* <Route path="/search" /> */}
+			{/* <Route path="/" element={<Home/>}/> */}
 		</Routes>
 	);
 }
